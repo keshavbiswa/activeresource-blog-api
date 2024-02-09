43ad52ad2d776f8758d757c88228e9c2bcee58ab
Keshav Biswa <keshavbiswa21@gmail.com>
2024-02-09 18:22:07 +0530

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |i|
+  User.create(
+    first_name: "User#{i}",
+    last_name: "User#{i}",
+    email: "test#{i}@example.com"
+  )
+end
+
+10.times do |i|
+  Post.create(
+    title: "Post#{i}",
+    content: "This is the body of post#{i}",
+    user_id: i
+  )
+end
+
+10.times do |i|
+  Comment.create(
+    content: "This is the body of comment#{i}",
+    post_id: i,
+    user_id: i
+  )
+end
+
